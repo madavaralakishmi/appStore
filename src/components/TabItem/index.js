5753643a8258f9b3d2252edf2093e4410c4b1d1f
madavaralakishmi <107978044+madavaralakishmi@users.noreply.github.com>
2022-07-15 15:32:18 +0530

    Update index.js

--- src/components/TabItem/index.js
+++ src/components/TabItem/index.js
@@ -1,19 +1,19 @@
 import './index.css'
 
 const TabItem = props => {
-  const {tabsList, onbutton} = props
+  const {tabsList, onbutton, isActive} = props
   const {displayText, tabId} = tabsList
 
   const onButtonClicked = () => {
     onbutton(tabId)
   }
+  const result = isActive ? ' btn-clicked ' : ''
 
   return (
     <li>
-      <button type="button" onClick={onButtonClicked}>
+      <button type="button" onClick={onButtonClicked} className={result}>
         {displayText}
       </button>
-      <hr />
     </li>
   )
 }
